39fc35b3f40b116ad7d165704fccb58ca6c9aa89
Benjamin Goodheart <107885125+benjamin-civi@users.noreply.github.com>
2023-07-13 14:28:19 -0400

    clean up

--- my-app/src/components/bv-example/BVCardComponent.vue
+++ my-app/src/components/bv-example/BVCardComponent.vue
@@ -1,19 +1,13 @@
 <script>
-export default{
-    name: "BVCardComponent",
-    setup(){
-        
-        return {}
-    }
+export default {
+  name: 'BVCardComponent',
+  setup() {
+    return {}
+  }
 }
 </script>
 <template>
-
-<b-card header="This is a Card" subtitle="with a subtitle">
-
-<b-card-text>
-Here's some <i>text</i>
-</b-card-text>
-</b-card>
-
+  <b-card header="This is a Card" subtitle="with a subtitle">
+    <b-card-text> Here's some <i>text</i> </b-card-text>
+  </b-card>
 </template>
--- my-app/src/components/bv-example/BVFormComponent.vue
+++ my-app/src/components/bv-example/BVFormComponent.vue
@@ -1,38 +1,37 @@
 <script>
-import {reactive} from "vue"
-export default{
-    name: 'BVFormComponent',
-    setup(){
-        const formData = reactive({
-            name: '',
-            comments: '',
-            performace: 50,
-        })
-        
-        const onSubmit = (event)=>{
-            event.preventDefault();
-            
-            console.log("/// Do something")
-        }
-        return {formData, onSubmit}
-    },
+import { reactive } from 'vue'
+export default {
+  name: 'BVFormComponent',
+  setup() {
+    const formData = reactive({
+      name: '',
+      comments: '',
+      performance: 50
+    })
+
+    const onSubmit = (event) => {
+      event.preventDefault()
+
+      console.log('/// Do something')
+    }
+    return { formData, onSubmit }
+  }
 }
 </script>
 
 <template>
-    <b-form @submit="onSubmit">
+  <b-form @submit="onSubmit">
     <b-form-group description="Personal Info">
-    <label>Name</label>
-    <b-form-input v-model="formData.name" placeholder="Your Name Here">
-    </b-form-input>
+      <label>Name</label>
+      <b-form-input v-model="formData.name" placeholder="Your Name Here"> </b-form-input>
     </b-form-group>
-    
+
     <b-form-group>
-    <label> Comments </label>
-    <b-form-textarea v-model="formData.comments" ></b-form-textarea>
-    <label>Performace Rating</label>
-    <b-form-input type="range" v-model="formData.performace"></b-form-input>
+      <label> Comments </label>
+      <b-form-textarea v-model="formData.comments"></b-form-textarea>
+      <label>Performace Rating</label>
+      <b-form-input type="range" v-model="formData.performance"></b-form-input>
     </b-form-group>
     <b-button variant="info" type="submit">Submit</b-button>
-    </b-form>
+  </b-form>
 </template>
